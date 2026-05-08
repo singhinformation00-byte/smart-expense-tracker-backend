ab642f8cc8dd2d46fb6ed55ef8a40a63fc3433fa
singhinformation00-byte <up81battlegroundclub@gmail.com>
2026-05-08 20:47:26 +0530

    fix production cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["https://yourdomain.com", "https://www.yourdomain.com"],
+    origin: [
+      "http://localhost:5173",
+      "https://expenza-ai.online",
+      "https://www.expenza-ai.online",
+    ],
     credentials: true,
   }),
 );
