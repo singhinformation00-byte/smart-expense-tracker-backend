8e58df55e38e7f073e7d077b5aa8b5d8fb5b891f
singhinformation00-byte <singhinformation.00@gmail.com>
2026-05-11 23:07:25 +0530

    Update sendEmail.js

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,7 +4,7 @@ const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true,
+    secure: false,
 
     auth: {
       user: process.env.EMAIL_USER,
