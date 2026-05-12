fbe80654112c4603d096ce3ba92b1ac9f6855b5a
singhinformation00-byte <up81battlegroundclub@gmail.com>
2026-05-12 16:46:18 +0530

    use gmail smtp

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,28 +2,13 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.hostinger.com",
-    port: 465,
-    secure: true,
+    service: "gmail",
 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-
-    family: 4,
-
-    connectionTimeout: 30000,
-    greetingTimeout: 30000,
-    socketTimeout: 30000,
-
-    tls: {
-      rejectUnauthorized: false,
-      minVersion: "TLSv1.2",
-    },
   });
-
-  await transporter.verify();
 
   await transporter.sendMail({
     from: `"Expenza AI" <${process.env.EMAIL_USER}>`,
