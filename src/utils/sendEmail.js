c8e5cd57ed5cc0dcbd6a053e868baaf4926767d4
singhinformation00-byte <up81battlegroundclub@gmail.com>
2026-05-12 17:52:44 +0530

    use resend email api

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "Expenza AI <noreply@expenza-ai.online>",
     to,
     subject,
     html,
