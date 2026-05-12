f391c9b882111c0a0be449c3799a5a3fc7da72c3
singhinformation00-byte <up81battlegroundclub@gmail.com>
2026-05-12 16:54:05 +0530

    force ipv4 gmail smtp

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,11 +2,19 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
+    },
+
+    family: 4,
+
+    tls: {
+      rejectUnauthorized: false,
     },
   });
 
