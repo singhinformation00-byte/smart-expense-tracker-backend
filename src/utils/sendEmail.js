18f9cb9af276dd35a13fd0d3a5100f72c9d5909c
singhinformation00-byte <up81battlegroundclub@gmail.com>
2026-05-12 09:16:34 +0530

    fix smtp

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,14 +3,22 @@ import nodemailer from "nodemailer";
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    port: 465,
     secure: true,
 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+
+    tls: {
+      rejectUnauthorized: false,
+    },
+
+    connectionTimeout: 10000,
   });
+
+  await transporter.verify();
 
   await transporter.sendMail({
     from: `"Expenza AI" <${process.env.EMAIL_USER}>`,
